6627d049f3f5edc8af6ce0c2580201a0bea51df9
paul lahana <paul.lahana@gmail.com>
2023-04-11 15:16:48 +0200

    fix on DB

--- db/migrate/20230410092803_add_image_data_to_photos.rb
+++
@@ -1,6 +0,0 @@
-class AddImageDataToOptionsAndInspirations < ActiveRecord::Migration[7.0]
-  def change
-    add_column :options, :image_data, :text
-    add_column :inspirations, :image_data, :text
-  end
-end
--- db/migrate/20230411091846_remove_image_from_option.rb
+++
@@ -1,6 +0,0 @@
-class RemoveImageFromOption < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :options, :image_data, :text
-    remove_column :inspirations, :image_data, :text
-  end
-end
