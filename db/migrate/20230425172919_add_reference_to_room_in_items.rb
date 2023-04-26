c4b9801f8fda92ebe4a73d9acf62350040717b97
paul lahana <paul.lahana@gmail.com>
2023-04-26 17:08:10 +0200

    adding kind before creating "other" room

--- db/migrate/20230425172919_add_reference_to_room_in_items.rb
+++ db/migrate/20230425172919_add_reference_to_room_in_items.rb
@@ -1,6 +1,7 @@
 class AddReferenceToRoomInItems < ActiveRecord::Migration[7.0]
   def change
     add_reference :items, :room, foreign_key: true
+    add_column :rooms, :kind, :integer, default: 0, null: false
 
     Item.all.each do |item|
       other = Room.create(name: 'other', flat_id: item.flat.id)
--- db/migrate/20230425193348_add_kind_to_room.rb
+++
@@ -1,5 +0,0 @@
-class AddKindToRoom < ActiveRecord::Migration[7.0]
-  def change
-    add_column :rooms, :kind, :integer, default: 0, null: false
-  end
-end
