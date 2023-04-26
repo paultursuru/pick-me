a6df8f63f3d632ad851e9f42735278be08c62e7c
paul lahana <paul.lahana@gmail.com>
2023-04-26 17:02:08 +0200

    flat.id instead of flat in add_reference migration

--- db/migrate/20230425172919_add_reference_to_room_in_items.rb
+++ db/migrate/20230425172919_add_reference_to_room_in_items.rb
@@ -3,7 +3,7 @@ class AddReferenceToRoomInItems < ActiveRecord::Migration[7.0]
     add_reference :items, :room, foreign_key: true
 
     Item.all.each do |item|
-      other = Room.create(name: 'other', flat: item.flat)
+      other = Room.create(name: 'other', flat_id: item.flat.id)
       item.room_id = other.id
       item.save!
     end
