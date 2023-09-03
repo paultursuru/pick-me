2acce6da195c7d69bddba2faea75d5c9c1fde876
paul lahana <paul.lahana@gmail.com>
2023-09-03 18:48:41 +0200

    fix on the scrapper now returning values and/or errors if any..

--- app/services/option_scrapper.rb
+++ app/services/option_scrapper.rb
@@ -3,59 +3,124 @@ class OptionScrapper < ApplicationService
   require "nokogiri"
 
   def initialize(options = {})
-    @url = options[:url]
+    @url      = options[:url]
+    @domain   = get_domain(@url)
+    @html_doc = nil
+    @error    = nil
   end
 
   def call
-    scrapp_url
+    scrapp_url       # if scrap works
+    if @error.nil?
+      build_option   # build and return option
+    else             # some website block scraping and we get a OpenURI::HTTPError (403 Forbidden)
+      {
+        status: "403",
+        website: @domain
+      }
+    end
   end
 
   private
 
   def scrapp_url
-    html_file = URI.open(@url).read
-    html_doc = Nokogiri::HTML.parse(html_file)
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0"
+    begin # try to OpenURI with a user-agent
+      html_file = URI.open(@url, "User-Agent" => user_agent).read
+    rescue OpenURI::HTTPError => error
+      # Handle HTTP errors here
+      @error = error
+    end
+    @html_doc = Nokogiri::HTML.parse(html_file)
+  end
 
-    case get_domain(@url)
+  def build_option
+    case @domain
     when "ikea"
-      Option.new with_ikea(html_doc)
+      Option.new with_ikea
     when "habitat"
-      Option.new with_habitat(html_doc)
+      Option.new with_habitat
     else
-      Option.new with_og(html_doc)
+      Option.new with_og
     end
   end
 
   def get_domain(url)
     url = "http://#{url}" if URI.parse(url).scheme.nil?
     host = URI.parse(url).host.downcase
-    (host.start_with?('www.') ? host[4..-1] : host).split(".").first
+    @domain = (host.start_with?('www.') ? host[4..-1] : host).split(".").first
   end
 
-  def with_ikea(html_doc)
+  def with_ikea
     {
-      name: html_doc.search(".pip-header-section__title--big").first.text.strip,
-      description: html_doc.search(".pip-header-section__description").first.text.strip,
-      price: html_doc.search(".pip-temp-price__integer").first.text.strip,
-      image_url: html_doc.search(".pip-image").first.attr('src')
+      name: @html_doc.search(".pip-header-section__title--big").first.text.strip,
+      description: @html_doc.search(".pip-header-section__description").first.text.strip,
+      price: @html_doc.search(".pip-temp-price__integer").first.text.strip,
+      image_url: @html_doc.search(".pip-image").first.attr('src')
     }
   end
 
-  def with_habitat(html_doc)
+  def with_habitat
     {
-      name: html_doc.search(".title-deco-4").first.text.strip,
-      description: html_doc.search(".product-wording-top__content").first.text.strip,
-      price: html_doc.search("meta[property='product:price:amount']").first.attributes["content"].value,
-      image_url: html_doc.css("meta[property='og:image']").first.attributes["content"].value
+      name: @html_doc.search(".title-deco-4").first.text.strip,
+      description: @html_doc.search(".product-wording-top__content").first.text.strip,
+      price: @html_doc.search("meta[property='product:price:amount']").first.attributes["content"].value,
+      image_url: @html_doc.css("meta[property='og:image']").first.attributes["content"].value
     }
   end
 
-  def with_og(html_doc)
+  def with_og
     {
-      name: html_doc.search("meta[property='og:title']").first.attributes["content"].value,
-      description: html_doc.search("meta[property='og:description']").first.attributes["content"].value,
-      price: html_doc.search("meta[property='og:price:amount']").first.attributes["content"].value,
-      image_url: html_doc.css("meta[property='og:image']").first.attributes["content"].value
+      name: safe_name,
+      description: safe_description,
+      price: safe_price,
+      image_url: safe_image_url
     }
+  end
+
+  def safe_name
+    if @html_doc.search("title")
+      @html_doc.search("title").first.content
+    elsif @html_doc.search("meta[property='og:title']")
+      @html_doc.search("meta[property='og:title']").first.attributes["content"].value
+    else
+      error_for("title")
+    end
+  end
+
+  def safe_description
+    if @html_doc.search("meta[name='description']")
+      @html_doc.search("meta[name='description']").first.attributes["content"].value
+    elsif @html_doc.search("meta[property='og:description']")
+      @html_doc.search("meta[property='og:description']").first.attributes["content"].value
+    else
+      error_for("description")
+    end
+  end
+
+  def safe_price
+    if !@html_doc.search("meta[name='price']").empty?
+      @html_doc.search("meta[name='price']").first.attributes["content"].value
+    elsif !@html_doc.search("meta[property='og:price']").empty?
+      @html_doc.search("meta[property='og:price']").first.attributes["content"].value
+    elsif !@html_doc.search("meta[property='product:price:amount']").empty?
+      @html_doc.search("meta[property='product:price:amount']").first.attributes["content"].value
+    else
+      error_for("price")
+    end
+  end
+
+  def safe_image_url
+    if !@html_doc.search("meta[property='image']").empty?
+      @html_doc.search("meta[property='image']").first.attributes["content"].value
+    elsif !@html_doc.search("meta[property='og:image']").empty?
+      @html_doc.search("meta[property='og:image']").first.attributes["content"].value
+    else
+      error_for("image")
+    end
+  end
+
+  def error_for(keyword)
+    "no #{keyword} found"
   end
 end
